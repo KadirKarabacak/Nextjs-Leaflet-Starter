c74ba82aaf10d74ebdc191c84db592db72b42849
Kadir <kadirht@hotmail.com>
2024-11-23 15:22:51 +0300

    package: Next.js Leaflet starter repository

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  //! Disabling React Strict Mode to avoid conflicts with the Map component
+  reactStrictMode: false,
 };
 
 export default nextConfig;
